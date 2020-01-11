802b72c11422f23c256c762f0f25daf4b1cb524c
adityaanand53 <adityaanand53@live.com>
2020-01-12 00:25:48 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const expresss = require('express');
+const puppeteer = require('puppeteer');
+const absolutify = require('absolutify');
+
+const app = expresss();
+
+app.get('/', async (req, res) => {
+    const {url} = req.query;
+    if (!url) {
+        res.send('No URL found');
+    } else {
+        try {
+            const browser = await puppeteer.launch({ignoreDefaultArgs: ['--disable-extensions'],});
+            const page = await browser.newPage();
+            await page.goto(`https://${url}`, {waitUntil: 'load', timeout: 0})
+            let document = await page.evaluate(() => document.documentElement.outerHTML, {waitUntil: 'load', timeout: 0});
+            document = absolutify(document, `/?url=${url.split('/')[0]}`)
+            res.send(document);
+            browser.close();
+        }
+        catch (err) {
+            console.log('Something went wrong, Error: ', err);
+            res.send('Unable to load page');
+            browser.close();
+        }
+      
+    }
+});
+app.listen(process.env.PORT);
